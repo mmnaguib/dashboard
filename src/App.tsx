7424862364e961505c897eca41b1d1619828c09a
mmnaguib <mnaguib126@gmail.com>
2024-12-18 16:16:46 +0200

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Categories from "./components/Categories/Categories";
+import Products from "./components/Products/Products";
+import Banners from "./components/Banners/Banners";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route index path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/banners" element={<Banners />} />
+          <Route index path="/" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/interface/index.ts
@@ -0,0 +1,5 @@
+export interface ILoginProps {}
+export interface ICategoryProps {}
+export interface IProductProps {}
+export interface IRegisterProps {}
+export interface IBannerProps {}
